4316222db831263ab68c037987bded37d253c1cf
Boris <boris.bordeaux@gmail.com>
2025-06-20 01:40:09 +0200

    Fixed colors for imbricated circles

--- shaders/plane/fs.glsl
+++ shaders/plane/fs.glsl
@@ -32,30 +32,33 @@ bool checkInCircle() {
 }
 
 void main() {
-    if(useColors == 0) {
+    if (useColors == 0) {
         if (nbVertices == 0 || checkInCircle()) {
             discard;
         }
         fragColor = vec4(0.8, 0.8, 0.8, 1.0);
     } else {
-        bool inCircle = false;
+        uint nbCircles = 0;
+        vec3 blendColor = vec3(0);
         for (uint i = 0; i < nbVertices; i += 6) {
             vec2 center = vec2(polyhedronVertices[i], polyhedronVertices[i + 1]);
             float radius = polyhedronVertices[i + 2];
             if (radius > 0) {
                 if (length(center - vertPos) < radius) { //test if in circle
-                    fragColor = vec4(polyhedronVertices[i + 3], polyhedronVertices[i + 4], polyhedronVertices[i + 5], 1.0);
-                    inCircle = true;
+                    blendColor += vec3(polyhedronVertices[i + 3], polyhedronVertices[i + 4], polyhedronVertices[i + 5]);
+                    nbCircles += 1;
                 }
             } else {
                 if (length(center - vertPos) > -radius) { //test if in circle
-                    fragColor = vec4(polyhedronVertices[i + 3], polyhedronVertices[i + 4], polyhedronVertices[i + 5], 1.0);
-                    inCircle = true;
+                    blendColor += vec3(polyhedronVertices[i + 3], polyhedronVertices[i + 4], polyhedronVertices[i + 5]);
+                    nbCircles += 1;
                 }
             }
         }
-        if (!inCircle) {
+        if (nbCircles == 0) {
             discard;
+        } else {
+            fragColor = vec4(blendColor / float(nbCircles), 1.0);
         }
     }
 }
